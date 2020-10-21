0116dd8cc9bff47c634c109eae61dafd55059f18
kuba64451 <60610934+kuba64451@users.noreply.github.com>
2020-10-21 15:52:23 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+image = {   "type": "",
+            "comment": "komentarz",
+            "size": [2, 3],
+            "pixels": [ [1,0],
+                        [0,1],
+                        [0,1 ]] 
+}
+
+def load_image(filename):
+    return image
+
+def save_image(filename, image):
+    pass
